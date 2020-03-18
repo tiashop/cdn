43b1aacce6251eab9b9108c7c32956e1f0790e31
tiashop <62321490+tiashop@users.noreply.github.com>
2020-03-18 12:47:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const images = document.querySelectorAll(".slide"),
+  next = document.querySelector(".next"),
+  prev = document.querySelector(".prev");
+
+let current = 0;
+
+function changeImage() {
+  images.forEach(img => {
+    img.classList.remove("show");
+    img.style.display = "none";
+  });
+
+  images[current].classList.add("show");
+  images[current].style.display = "block";
+}
+
+// Calling first time
+changeImage();
+
+next.addEventListener("click", function() {
+  current++;
+
+  if (current > images.length - 1) {
+    current = 0;
+  } else if (current < 0) {
+    current = images.length - 1;
+  }
+
+  changeImage();
+});
+prev.addEventListener("click", function() {
+  current--;
+
+  if (current > images.length - 1) {
+    current = 0;
+  } else if (current < 0) {
+    current = images.length - 1;
+  }
+
+  changeImage();
+});
+
+// Auto change in 5 seconds
+
+setInterval(() => {
+  next.click();
+}, 5000);
